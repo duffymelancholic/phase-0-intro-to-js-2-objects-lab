6457f07066a78fb0351ca0101c73db9ea8504fc2
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-06-09 14:57:48 +0300

    Complete with  everything

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+
+
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  // Export all if using Node.js and tests are in separate file
+  module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+  };
